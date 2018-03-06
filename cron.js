574a203eeaea96a4309e70c6fe0262d9b021e7e2
osamtimizer <eaten.jb@gmail.com>
2018-03-07 01:27:29 +0900

    add credential info of firebase(service key isn't added)

--- cron.js
+++ cron.js
@@ -1,4 +1,20 @@
+const firebase_admin = require('firebase_admin');
+const serviceAccount = require('./ikaassistant-auth-key.json');
+
+firebase_admin.initializeApp({
+  credential: firebase_admin.credential.cert(serviceAccount),
+  databaseURL: "https://ikaassistant-e5b63.firebaseio.com"
+});
+
 let cron = require('node-cron');
+//this task must be executed every 2 hours.
 cron.schedule('* * * * * *', () => {
   console.log("hello nodejs");
+  //prepare HTTP request
+  //
+  //send request
+  //
+  //parse json from server
+  //
+  //refresh stage info on firebase realtimedb
 });
