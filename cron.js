1e08e32dd0b4e269a8b6d56f2ded56955eeb2597
osamtimizer <eaten.jb@gmail.com>
2018-03-07 10:49:28 +0900

    wrote send request to API server

--- cron.js
+++ cron.js
@@ -10,23 +10,44 @@ firebase_admin.initializeApp({
 let cron = require('node-cron');
 //this task must be executed every 2 hours.
 cron.schedule('* * * * * *', () => {
-  console.log("hello nodejs");
   //prepare HTTP request
   const iksm_session = process.env.IKSM_SESSION;
-  console.log(iksm_session);
 
   const cookie = "iksm_session=" + iksm_session;
   const url = "https://app.splatoon2.nintendo.net/api/schedules";
-  const header = {
-    url: url,
+  const headers = {
     Cookie: cookie,
-    method: "GET"
   };
 
-  //
+  const options = {
+    url: url,
+    method: "GET",
+    headers: headers
+  };
+
   //send request
-  //
+  request(options).then((result) => {
+    console.log("json obj fetched");
+    console.log(ISON.parse(result));
+  }).catch((err) => {
+    console.error("Error: ", err);
+    return false;
+  });
+
   //parse json from server
   //
   //refresh stage info on firebase realtimedb
+  const db = firebase_admin.database();
+  const ref = db.ref('stages').push();
+  ref.set({
+    stage_name: "TEST",
+    start_time: "21:00",
+    date: "0307"
+  }, (err) => {
+    if (err) {
+      console.error("Error: ", err);
+    } else {
+      console.log("success");
+    }
+  });
 });
