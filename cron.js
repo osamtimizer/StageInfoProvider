c51da8855ee567e64e89f0005e49dd6c102d74d0
osamtimizer <eaten.jb@gmail.com>
2018-03-07 23:44:15 +0900

    move db access into the if condition

--- cron.js
+++ cron.js
@@ -16,47 +16,54 @@ cron.schedule('* * * * * *', () => {
   const iksm_session = process.env.IKSM_SESSION;
 
   const cookie = "iksm_session=" + iksm_session;
-  const ua = "StageInfoProvider/0.1 (twitter @osamtimizer)";
+  const user_agent = "StageInfoProvider/0.1 (twitter @osamtimizer)";
 
   //This url should be changed to other unofficial API server because the Cookie will be expired within 48 hours on official API server.
   const url = "https://app.splatoon2.nintendo.net/api/schedules";
-  const url_unofficial = "https://spla2.yuu26.com/schedule";
+  const url_unofficial_schedule_battle = "https://spla2.yuu26.com/schedule";
+  const url_unofficial_schedule_albeit = "https://spla2.yuu26.com/coop/schedule";
 
   //Cookie(iksm_session) is not required for unofficial API server.
   //User-Agent should be set instead.
   const headers = {
-    "User-Agent": ua
+    "User-Agent": user_agent
   };
 
   const options = {
-    url: url_unofficial,
+    url: url_unofficial_schedule_battle,
     method: "GET",
     headers: headers
   };
 
   //send request
   request(options).then((result) => {
-    console.log("json obj fetched");
-    fs.writeFile("result.json", result);
+    console.log("json obj has been fetched correctly.");
+    //parse json from server
+    //
+    //refresh stage info on firebase realtimedb
+    const db = firebase_admin.database();
+    const ref = db.ref('stages').push();
+    //check whether the latest info is already pushed.
+    //TODO: compare info between json and db.
+    if (true) {
+      ref.set({
+        rule: "gachi"
+        stage_name: "TEST",
+        start_time: "21:00",
+        date: "0307"
+      }, (err) => {
+        if (err) {
+          console.error("Error: ", err);
+        } else {
+          console.log("success");
+        }
+      });
+    } else {
+      //nothing to do.
+    }
   }).catch((err) => {
     console.error("Error: ", err);
     return false;
   });
 
-  //parse json from server
-  //
-  //refresh stage info on firebase realtimedb
-  const db = firebase_admin.database();
-  const ref = db.ref('stages').push();
-  ref.set({
-    stage_name: "TEST",
-    start_time: "21:00",
-    date: "0307"
-  }, (err) => {
-    if (err) {
-      console.error("Error: ", err);
-    } else {
-      console.log("success");
-    }
-  });
 });
